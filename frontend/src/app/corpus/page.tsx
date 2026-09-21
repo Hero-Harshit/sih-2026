9a692a47c453d79e7a7fe508391dcb8c78a3e9fe
Harshit <heroharshitlaptop@gmail.com>
2026-09-22 04:10:44 +0530

    Corpus populated

--- frontend/src/app/corpus/page.tsx
+++ frontend/src/app/corpus/page.tsx
@@ -8,6 +8,7 @@ import {
   Search,
   Library,
   Scale,
+  FileText,
 } from "lucide-react";
 import { useLanguage } from "@/i18n/LanguageContext";
 
@@ -26,6 +27,341 @@ export default function CorpusPage() {
     "https://indiacode.nic.in",
     "https://ipindia.gov.in",
     "https://drive.google.com/drive/folders/1GbY-el4SGrOmSisolK4dkfU9Lmeo1CBJ?usp=drive_link",
+  ];
+
+  const groundedDatabaseDocs = [
+    {
+      name: "Trade_Marks_Act_1999.pdf",
+      url: "https://drive.google.com/open?id=1EQTdihuhlNusdVJ9qmw5ytOmQeGpMLr1&usp=drive_copy"
+    },
+    {
+      name: "Patent_Act_1970.pdf",
+      url: "https://drive.google.com/file/d/1OKFikAtUslaSnCoJEUByHipNUiCxcGrW/view?usp=drive_link"
+    },
+    {
+      name: "Geographical_Indications_Act_1999.pdf",
+      url: "https://drive.google.com/file/d/1bUNt96fW-37snoWVgvW5AwVPyAwmXrWu/view?usp=drive_link"
+    },
+    {
+      name: "NBA_Financial_Formulas_Exemptions.pdf",
+      url: "https://drive.google.com/file/d/1lc6cIxbQ6aDTPoLyGs9Hv-IfCIjahRTz/view?usp=drive_link"
+    },
+    {
+      name: "Biological_Diversity_Rules_2024_V2.pdf",
+      url: "https://drive.google.com/file/d/1By5nLo1VAuUpCGaVT3dZZipnD6FhRvpK/view?usp=drive_link"
+    },
+    {
+      name: "Biological_Diversity_Rules_2024_V1.pdf",
+      url: "https://drive.google.com/file/d/10qaLaXzgpcYEySSDNtTFSDB7RYfoK72P/view?usp=drive_link"
+    },
+    {
+      name: "Biological_Diversity_Act_2002.pdf",
+      url: "https://drive.google.com/file/d/1UeOG5r_OZXl0Jm6kvWffA89_So59n6Sk/view?usp=drive_link"
+    },
+    {
+      name: "Packaging_Requirements.pdf",
+      url: "https://drive.google.com/file/d/1ixphLrlAzySCgRIkA7Om81oPuyLfS68w/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Magic_Remedies_Act_Section_6.pdf",
+      url: "https://drive.google.com/file/d/1FhKBjHevtHdKPogcdsgawKHYr4B4KmEn/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Magic_Remedies_Act_Section_5.pdf",
+      url: "https://drive.google.com/file/d/1U8wKlhgPzN31BIfgr5WRF210sFA02B-L/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Magic_Remedies_Act_Section_4.pdf",
+      url: "https://drive.google.com/file/d/1nj2AwUK2NUMS47BllQyFflb007kJwnd6/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Magic_Remedies_Act_Section_3.pdf",
+      url: "https://drive.google.com/file/d/1ojKdBxxhy9QaZh8OUk9qK9cyzWU1-Pwj/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Magic_Remedies_Act_Section_2.pdf",
+      url: "https://drive.google.com/file/d/10Njm5eyTuT9_nMi_An4lmX-QNFWoB7nT/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Magic_Remedies_Act_Section_1.pdf",
+      url: "https://drive.google.com/file/d/1WZn__qCz58OSubNBfN_7AzdWLB8V5JI4/view?usp=drive_link"
+    },
+    {
+      name: "ASCI_Specific_Guidelines.pdf",
+      url: "https://drive.google.com/file/d/1IqjJpVdJcx3it3uhHiv67vCy3eaalcJO/view?usp=drive_link"
+    },
+    {
+      name: "ASCI_Advertising_Standards_Part_6.pdf",
+      url: "https://drive.google.com/file/d/1ImZLyXykzedBPJ2g2Wyk201tetQX6H8e/view?usp=drive_link"
+    },
+    {
+      name: "ASCI_Advertising_Standards_Part_5.pdf",
+      url: "https://drive.google.com/file/d/1b4LNUBMIJ-MRclZ3YSUxNkgZKpZ2wVmZ/view?usp=drive_link"
+    },
+    {
+      name: "ASCI_Advertising_Standards_Part_4.pdf",
+      url: "https://drive.google.com/file/d/1gWDZYzmDh0pY4ozdaDEZUruXWa7vlbdJ/view?usp=drive_link"
+    },
+    {
+      name: "ASCI_Advertising_Standards_Part_3.pdf",
+      url: "https://drive.google.com/file/d/12ApXF7FYB8aivcZvMNWZsO-5OZJJuLl2/view?usp=drive_link"
+    },
+    {
+      name: "ASCI_Advertising_Standards_Part_2.pdf",
+      url: "https://drive.google.com/file/d/1L4GFRUnoVSg0EPDCjUGN1pgvlrHfwORw/view?usp=drive_link"
+    },
+    {
+      name: "ASCI_Advertising_Standards_Part_1.pdf",
+      url: "https://drive.google.com/file/d/1z-qwa5hDnkz3XTZfJT71FdE6OJowzk6E/view?usp=drive_link"
+    },
+    {
+      name: "ASCI_Advertising_Standards_Main.pdf",
+      url: "https://drive.google.com/file/d/16fP9UMdM6y2HXQtFAWXu-wl8VwBa0_Hc/view?usp=drive_link"
+    },
+    {
+      name: "Nagoya_Protocol.pdf",
+      url: "https://drive.google.com/file/d/1YWrQDw2201zyFIBqfyAd4iJHLE9arMzW/view?usp=drive_link"
+    },
+    {
+      name: "Patent_Cooperation_Treaty_PCT.pdf",
+      url: "https://drive.google.com/file/d/1sLS0_ie1w_CZ9PE3Q3Wph3xvm-KLUAip/view?usp=drive_link"
+    },
+    {
+      name: "WIPO_GRATK_Treaty.pdf",
+      url: "https://drive.google.com/file/d/1VSTN9VasQk1wqXg0DTlh6S55QyyqC3jU/view?usp=drive_link"
+    },
+    {
+      name: "Budapest_Treaty_Regulations.pdf",
+      url: "https://drive.google.com/file/d/10-mry7H-WlJ1Wj1BJhLBr05zCLICfljv/view?usp=drive_link"
+    },
+    {
+      name: "Budapest_Treaty_Articles.pdf",
+      url: "https://drive.google.com/file/d/16ILbqT4G7QYG7t4WCYdZ-rB7DIq6Z4aG/view?usp=drive_link"
+    },
+    {
+      name: "EU_Directive_2001_83_EC_Medicinal_Product_Advertising.pdf",
+      url: "https://drive.google.com/file/d/1yTEo5B11vvr1GBAwA4Vs_GTI_x_VkuMX/view?usp=drive_link"
+    },
+    {
+      name: "EU_Directive_2001_83_EC_Labelling.pdf",
+      url: "https://drive.google.com/file/d/1cKkXIp-TSdRbwgFQu-NcGpg2xN5CEPsn/view?usp=drive_link"
+    },
+    {
+      name: "EU_Directive_2001_83_EC_Human_Medicinal_Products.pdf",
+      url: "https://drive.google.com/file/d/1hP3wVK0F-X3KZ__vZRYA0TGEOOYgB7Zc/view?usp=drive_link"
+    },
+    {
+      name: "EU_Directive_2001_83_EC_Advertising.pdf",
+      url: "https://drive.google.com/file/d/1VrNANHP2eW9mRaOodS7Xw6_PgMzonJhJ/view?usp=drive_link"
+    },
+    {
+      name: "EU_Cosmetic_Regulation_EC_1223_2009_Part_5.pdf",
+      url: "https://drive.google.com/file/d/1vKgbwO15dy0cVXnyb1-BwXhSe2EfyD6I/view?usp=drive_link"
+    },
+    {
+      name: "EU_Cosmetic_Regulation_EC_1223_2009_Part_2.pdf",
+      url: "https://drive.google.com/file/d/1_KvtIOtPwiVCV1jd4moTku-RkGtvCKE1/view?usp=drive_link"
+    },
+    {
+      name: "EU_Cosmetic_Regulation_EC_1223_2009_Part_1.pdf",
+      url: "https://drive.google.com/file/d/10d3FFZ7bawAyw64MfARY1f_-HjUU_Qc2/view?usp=drive_link"
+    },
+    {
+      name: "EU_Cosmetic_Regulation_EC_1223_2009_Main.pdf",
+      url: "https://drive.google.com/file/d/1QvKiuZwWt3xXmMvQ3uMPArLJyu5qtVCu/view?usp=drive_link"
+    },
+    {
+      name: "TRIPS_Trademark.pdf",
+      url: "https://drive.google.com/file/d/1GIdAxY9NtBJpXEvufpu2w40VwaPcDUTV/view?usp=drive_link"
+    },
+    {
+      name: "TRIPS_Patent.pdf",
+      url: "https://drive.google.com/file/d/1axbXX6PeOdByhDDAleFHQ4zWf3nXzvz4/view?usp=drive_link"
+    },
+    {
+      name: "TRIPS_Geographical_Indication.pdf",
+      url: "https://drive.google.com/file/d/1cMtFhGEjOCO1knlDi_AjEqYME46ZwzeO/view?usp=drive_link"
+    },
+    {
+      name: "US_Importation_FDA_Enforcement_Part_1.pdf",
+      url: "https://drive.google.com/file/d/15jxczpzs9K4jJ5wKfPQqMnGW787FIi4b/view?usp=drive_link"
+    },
+    {
+      name: "US_Importation_FDA_Enforcement_Part_1_Duplicate.pdf",
+      url: "https://drive.google.com/file/d/1Gqqvh17RyoPy66jcBg_hQrw8eCmbUlvP/view?usp=drive_link"
+    },
+    {
+      name: "US_Importation_FDA_Enforcement_Main.pdf",
+      url: "https://drive.google.com/file/d/1FOLPGs8I7-v25V7JaQI2zNGwWtZIXrO8/view?usp=drive_link"
+    },
+    {
+      name: "US_FTC_Part_1.pdf",
+      url: "https://drive.google.com/file/d/1MFpA2DnW7Yn0zTnCqd664yUFK6lmt32G/view?usp=drive_link"
+    },
+    {
+      name: "US_FTC_Main.pdf",
+      url: "https://drive.google.com/file/d/10HSd_t1RnKOYa870Okj0ZeTxtcFq-SH_/view?usp=drive_link"
+    },
+    {
+      name: "US_FTC_Health_Claims_Examples_Part_3.pdf",
+      url: "https://drive.google.com/file/d/13rtyEkr_4Ys1v70IjIZk1JXaeV2UtZCs/view?usp=drive_link"
+    },
+    {
+      name: "US_FTC_Health_Claims_Examples_Part_2.pdf",
+      url: "https://drive.google.com/file/d/1W8vJGW57PcoSnXa5f2VNR8idhZUfDxo0/view?usp=drive_link"
+    },
+    {
+      name: "US_FTC_Health_Claims_Examples_Part_1.pdf",
+      url: "https://drive.google.com/file/d/16bJSU9ozSM_BTa_p7xe3sp6HQgehuqoR/view?usp=drive_link"
+    },
+    {
+      name: "US_FTC_Health_Claims_Examples_Main.pdf",
+      url: "https://drive.google.com/file/d/1BOAWKAoFU5zRx5vyVySj6Mt60sfKPNgr/view?usp=drive_link"
+    },
+    {
+      name: "US_FDC_Act_Part_4.pdf",
+      url: "https://drive.google.com/file/d/1rKVproML2iU33MeFSTv-Lzeqm9oAWZKO/view?usp=drive_link"
+    },
+    {
+      name: "US_FDC_Act_Part_3.pdf",
+      url: "https://drive.google.com/file/d/1alPcJXBiQWDem24KgptMOC4l4lExG5qV/view?usp=drive_link"
+    },
+    {
+      name: "US_FDC_Act_Part_2.pdf",
+      url: "https://drive.google.com/file/d/1JR0S94zyVDn0DAsE9dWBs2hvRn2AXzHZ/view?usp=drive_link"
+    },
+    {
+      name: "US_FDC_Act_Part_1.pdf",
+      url: "https://drive.google.com/file/d/1Y-dLimihGZVblp2-ZPpKNIPTRoqcOXnu/view?usp=drive_link"
+    },
+    {
+      name: "US_DSHEA_Dietary_Supplements.pdf",
+      url: "https://drive.google.com/file/d/1vSnYACxxXLsO1LlYvJ1nmWYj3dMFfIYI/view?usp=drive_link"
+    },
+    {
+      name: "US_DSHEA_Dietary_Supplements_Part_2.pdf",
+      url: "https://drive.google.com/file/d/1UfC-7_nit1PnrppFi-7eHAicsHp-jBYi/view?usp=drive_link"
+    },
+    {
+      name: "US_DSHEA_Dietary_Supplements_Part_1.pdf",
+      url: "https://drive.google.com/file/d/1GV9xv654LR6ouJXnOGzH-ekqw760IaRL/view?usp=drive_link"
+    },
+    {
+      name: "US_Cosmetics_FDC_MoCRA_Part_3.pdf",
+      url: "https://drive.google.com/file/d/15VJ2_bCWbhIK1rvPzdmt61uVFK9hSmI3/view?usp=drive_link"
+    },
+    {
+      name: "US_Cosmetics_FDC_MoCRA_Part_2.pdf",
+      url: "https://drive.google.com/file/d/13J8nb9IcKQ5Xb9BAGpNmpoQnpMp9bkIg/view?usp=drive_link"
+    },
+    {
+      name: "US_Cosmetics_FDC_MoCRA_Part_1.pdf",
+      url: "https://drive.google.com/file/d/19-Q5vtDe4pE6hZi9gX_R8ItuB98Hna8f/view?usp=drive_link"
+    },
+    {
+      name: "US_Cosmetics_FDC_MoCRA_Main.pdf",
+      url: "https://drive.google.com/file/d/1Ri98rCy1kViEPl-0SaucArRlih2OhW9Z/view?usp=drive_link"
+    },
+    {
+      name: "Ayurveda_Ayush_Regulations.pdf",
+      url: "https://drive.google.com/file/d/1lqz6DFc1jhZrvZNEKctdekfI40NZ7vjv/view?usp=drive_link"
+    },
+    {
+      name: "Biodiversity_and_ABS.pdf",
+      url: "https://drive.google.com/file/d/1wvRKwmqcvbOnFLXhPGbEPW4jdXFf32gH/view?usp=drive_link"
+    },
+    {
+      name: "CSIRs_Turmeric_Patent_Revocation.pdf",
+      url: "https://drive.google.com/file/d/1PUNlniNr7AfELpq4WbrxBy8AaNSHjS8E/view?usp=drive_link"
+    },
+    {
+      name: "Dabur_v._Aveda.pdf",
+      url: "https://drive.google.com/file/d/1mZa1yhWtucBo8sko8BvEzs75FwapIDH3/view?usp=drive_link"
+    },
+    {
+      name: "Dabur_v._Patanjali.pdf",
+      url: "https://drive.google.com/file/d/1J3VGZbBw7KOUBom-uOHtl45kcPgGo_xR/view?usp=drive_link"
+    },
+    {
+      name: "Divya_Pharmacy_v._Union_of_India.pdf",
+      url: "https://drive.google.com/file/d/1XDPOxdEB4YrgY8O0VLJ0z3zwGaftXggt/view?usp=drive_link"
+    },
+    {
+      name: "Emami_v._Dabur.pdf",
+      url: "https://drive.google.com/file/d/11W8wLFKfuTIo1XJCvOg7SbTaQk6afc1f/view?usp=drive_link"
+    },
+    {
+      name: "Food_Nutraceutical_Side.pdf",
+      url: "https://drive.google.com/file/d/1lVTpZCDEJ5_4e3bUTTK5eNVYtS7uvJR_/view?usp=drive_link"
+    },
+    {
+      name: "GI_Copyright_Brand.pdf",
+      url: "https://drive.google.com/file/d/16hq1xzK3VWrMN07uByh64HRuVT9eSkXP/view?usp=drive_link"
+    },
+    {
+      name: "International_Law.pdf",
+      url: "https://drive.google.com/file/d/1syWIf60cvdGoWolp1WYIsj_E4pBY-3LS/view?usp=drive_link"
+    },
+    {
+      name: "Neem_Patent_Revocation.pdf",
+      url: "https://drive.google.com/file/d/1NtKjdhYGsELsNBy2nclu_wKDDqhKh5HN/view?usp=drive_link"
+    },
+    {
+      name: "Patent_Law.pdf",
+      url: "https://drive.google.com/file/d/1pL59WvQ0qkJg50kYMNNVXsmPlpMHG4KK/view?usp=drive_link"
+    },
+    {
+      name: "RiceTec_Basmati_Patent.pdf",
+      url: "https://drive.google.com/file/d/1AxlXveUgdBKk8rjSF71ypE8bJaI8ow3F/view?usp=drive_link"
+    },
+    {
+      name: "TKDL_Prior_Art_Defeats_a_Foreign_Patent_Application.pdf",
+      url: "https://drive.google.com/file/d/1fpSE5sp2nlaSQn6x9JdRckYe3Q--DLiv/view?usp=drive_link"
+    },
+    {
+      name: "Trade_Mark_Brand.pdf",
+      url: "https://drive.google.com/file/d/1WNgIRFR7NkU4qVDRMbMJ2P64aO8Xl1lS/view?usp=drive_link"
+    },
+    {
+      name: "Traditional_Knowledge.pdf",
+      url: "https://drive.google.com/file/d/13fh93ki2UbsCrKzuPg1jyoiOmcUPMnUy/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Cosmetics_Rules_1945_Part_7.pdf",
+      url: "https://drive.google.com/file/d/11p6OE0b4YIRljYHAr4sdTsoWmwjGPazp/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Cosmetics_Rules_1945_Part_6.pdf",
+      url: "https://drive.google.com/file/d/1TTIz5LTtBJaTQTn2susRFH9FQTTHaK6Z/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Cosmetics_Rules_1945_Part_5.pdf",
+      url: "https://drive.google.com/file/d/1GtPzJisIl6F47ln4cIHwblnb8jtBas6L/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Cosmetics_Rules_1945_Part_4.pdf",
+      url: "https://drive.google.com/file/d/1wVxCnJ_zx3tGHq-1VF2Gy60sYJtjBQPm/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Cosmetics_Rules_1945_Part_3.pdf",
+      url: "https://drive.google.com/file/d/1Kuiu1ROQDm5YQdozKZb7-Ai-XQQkyPgX/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Cosmetics_Rules_1945_Part_2.pdf",
+      url: "https://drive.google.com/file/d/1PFHH5BsRNtazbddQNOE70Xkpx8O9cj0_/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Cosmetics_Rules_1945_Part_1.pdf",
+      url: "https://drive.google.com/file/d/1FOJVyF-SUBaSipDmdLLkCoYK9AjJr5He/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Cosmetics_Law_Part_2.pdf",
+      url: "https://drive.google.com/file/d/1UO9aJeYhFG-5X3OIB2WxAi8usLd7ep7T/view?usp=drive_link"
+    },
+    {
+      name: "Drugs_And_Cosmetics_Law_Part_1.pdf",
+      url: "https://drive.google.com/file/d/1xBN-EifZAgksJE_hqExhYFSLDLi7NN_A/view?usp=drive_link"
+    }
   ];
 
   return (
@@ -60,7 +396,7 @@ export default function CorpusPage() {
                     href={sourceUrls[idx]}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="group relative flex flex-col p-6 rounded-3xl bg-white dark:bg-slate-900 border border-slate-100 dark:border-slate-800 shadow-sm hover:shadow-xl hover:-translate-y-1 transition-all duration-300 overflow-hidden"
+                    className="group relative flex flex-col p-6 rounded-3xl bg-white dark:bg-slate-900 border border-brand-500 shadow-sm hover:shadow-xl hover:-translate-y-1 transition-all duration-300 overflow-hidden"
                   >
                     {/* Decorative gradient blur */}
                     <div className="absolute top-0 right-0 w-32 h-32 bg-gradient-to-bl from-brand-100 to-transparent rounded-full opacity-50 blur-2xl group-hover:opacity-100 transition-opacity"></div>
@@ -85,6 +421,36 @@ export default function CorpusPage() {
                       </div>
                     </div>
                   </a>
+                ))}
+              </div>
+            </div>
+
+            {/* Our Robust Grounded Database */}
+            <div className="space-y-6 animate-in slide-in-from-bottom-4 fade-in duration-300" style={{ animationDelay: "200ms" }}>
+              <h2 className="text-2xl font-bold text-slate-800 dark:text-slate-100 flex items-center gap-2">
+                <FileText className="w-6 h-6 text-brand-500" />
+                Our Robust Grounded Database
+              </h2>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+                {[...groundedDatabaseDocs].sort((a, b) => a.name.localeCompare(b.name)).map((doc, idx) => (
+                  <div key={idx} className="flex items-center justify-between p-5 rounded-2xl bg-white dark:bg-slate-900 border border-slate-100 dark:border-slate-800 shadow-sm hover:shadow-md hover:-translate-y-1 transition-all duration-300 group">
+                    <div className="flex items-center gap-4 min-w-0">
+                      <div className="w-10 h-10 rounded-xl bg-brand-50 flex items-center justify-center text-brand-600 shrink-0 group-hover:scale-110 transition-transform duration-300">
+                        <FileText className="w-5 h-5" />
+                      </div>
+                      <p className="font-semibold text-slate-700 dark:text-slate-200 truncate text-sm min-w-0">
+                        {doc.name}
+                      </p>
+                    </div>
+                    <a
+                      href={doc.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="shrink-0 ml-4 px-4 py-2 rounded-xl border-2 border-brand-500 text-brand-600 hover:bg-brand-50 hover:text-brand-700 font-medium text-sm transition-colors"
+                    >
+                      Access File
+                    </a>
+                  </div>
                 ))}
               </div>
             </div>
